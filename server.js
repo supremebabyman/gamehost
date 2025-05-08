123ffcf107a52218f9d58bd549fdcb89116bc6e6
Radio-A <166443294+Radio-A@users.noreply.github.com>
2025-05-07 23:35:54 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname)));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+// Optional: handle 404.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '404.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
