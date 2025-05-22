faf23926b4fb84efac7ea6958824dcc576a95e69
Dannen Plumley <166443294+Radio-A@users.noreply.github.com>
2025-05-22 17:54:44 +0000

    praying

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fetch = require("node-fetch");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3040;
 const port = PORT;
 // Using node-fetch v2
 
