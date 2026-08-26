6cc467f7f32f5a0f198580162fe34e6e4e818457
Supreme-s-alt <fsasidwtsut@gmail.com>
2026-08-26 21:19:39 +0000

    added checks

--- script.js
+++ script.js
@@ -1822,7 +1822,10 @@ document.addEventListener("DOMContentLoaded", () => {
 function crossPlatURLGrabber() {
     const parentUrl = encodeURIComponent(window.location.href);
     const frame = document.getElementById('player');
-    frame.src = `https://scramjet-o9kx.onrender.com/#?host=${parentUrl}`;
+    if (parentUrl !== null); {
+      frame.src = `https://scramjet-o9kx.onrender.com/#?host=${parentUrl}`;
+      console.log (parentUrl)
+    }
 }
 
 crossPlatURLGrabber();
